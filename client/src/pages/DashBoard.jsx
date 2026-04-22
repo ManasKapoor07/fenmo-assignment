691832b545d2d3e382e3a654af002d9c2ab0df58
Manas Kapoor <manaskapoor@Manass-MacBook-Air.local>
2026-04-22 21:47:09 +0530

    clean up

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -30,7 +30,7 @@ export default function Dashboard() {
       <div className="max-w-3xl mx-auto space-y-6">
 
         <h1 className="text-3xl font-bold text-center">
-          💸 Expense Tracker
+           Expense Tracker
         </h1>
 
         <div className="bg-white p-5 rounded-xl shadow">
